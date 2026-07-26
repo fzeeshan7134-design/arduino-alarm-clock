d5d63c836a3923cf704ad4f35f7cfa25a0bf0dc9
fzeeshan7134-design <f.zeeshan7134@gmail.com>
2026-07-26 18:28:55 -0400

    Improve button handling and adjust quick test duration
    Refactor button handling and update alarm timing.

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -2,7 +2,7 @@
 #include <RF24.h>
 #include <LiquidCrystal.h>
 
-RF24 radio(7, 8); 
+RF24 radio(7, 8);
 const byte address[6] = "ALARM";
 
 LiquidCrystal lcd(9, 10, A0, A1, A2, A3);
@@ -13,7 +13,7 @@ const int BTN_UP = A5;
 const int BUZZER = 6;
 
 int setHours = 0;
-int setMinutes = 5; 
+int setMinutes = 5;
 
 bool armed = false;
 unsigned long alarmTargetMillis = 0;
@@ -28,7 +28,7 @@ const unsigned long DEBOUNCE_MS = 200;
 bool setButtonHeld = false;
 unsigned long setPressStartMillis = 0;
 const unsigned long LONG_PRESS_MS = 1500;
-const unsigned long QUICK_TEST_MS = 60000UL; 
+const unsigned long QUICK_TEST_MS = 5000UL;
 
 void setup() {
   Serial.begin(9600);
@@ -52,37 +52,40 @@ void setup() {
 void loop() {
   handleButtons();
   checkAlarmTrigger();
+
   if (alarmActive) {
     ringBuzzer();
     checkForShutoffSignal();
   }
+
   updateDisplay();
 }
 
 void handleButtons() {
-
   bool setPressed = digitalRead(BTN_SET) == LOW;
 
   if (setPressed && !setButtonHeld) {
     setButtonHeld = true;
     setPressStartMillis = millis();
-  } else if (!setPressed && setButtonHeld) {
+  } 
+  else if (!setPressed && setButtonHeld) {
     setButtonHeld = false;
     unsigned long heldFor = millis() - setPressStartMillis;
 
     if (heldFor >= LONG_PRESS_MS) {
-
       alarmTargetMillis = millis() + QUICK_TEST_MS;
       armed = true;
       alarmActive = false;
       currentMode = NORMAL;
       noTone(BUZZER);
-    } else {
+    } 
+    else {
       currentMode = SET_HOURS;
       armed = false;
       alarmActive = false;
       noTone(BUZZER);
     }
+
     lastButtonPress = millis();
     return;
   }
@@ -93,18 +96,25 @@ void handleButtons() {
     if (digitalRead(BTN_UP) == LOW) {
       lastButtonPress = millis();
       setHours = (setHours + 1) % 24;
-    } else if (digitalRead(BTN_NEXT) == LOW) {
+    } 
+    else if (digitalRead(BTN_NEXT) == LOW) {
       lastButtonPress = millis();
       currentMode = SET_MINUTES;
     }
-  } else if (currentMode == SET_MINUTES) {
+  } 
+  
+  else if (currentMode == SET_MINUTES) {
     if (digitalRead(BTN_UP) == LOW) {
       lastButtonPress = millis();
       setMinutes = (setMinutes + 1) % 60;
-    } else if (digitalRead(BTN_NEXT) == LOW) {
+    } 
+    else if (digitalRead(BTN_NEXT) == LOW) {
       lastButtonPress = millis();
-      alarmTargetMillis = millis() + (unsigned long)setHours * 3600000UL
-                                    + (unsigned long)setMinutes * 60000UL;
+
+      alarmTargetMillis = millis() + 
+        (unsigned long)setHours * 3600000UL +
+        (unsigned long)setMinutes * 60000UL;
+
       armed = true;
       currentMode = NORMAL;
     }
@@ -120,18 +130,23 @@ void checkAlarmTrigger() {
 void ringBuzzer() {
   unsigned long cyclePos = millis() % 1000;
   int freq;
+
   if (cyclePos < 500) {
     freq = map(cyclePos, 0, 500, 500, 1500);
-  } else {
+  } 
+  else {
     freq = map(cyclePos, 500, 1000, 1500, 500);
   }
+
   tone(BUZZER, freq);
 }
 
 void checkForShutoffSignal() {
   if (radio.available()) {
     char msg[16] = "";
+
     radio.read(&msg, sizeof(msg));
+
     if (strcmp(msg, "SHUTOFF") == 0) {
       alarmActive = false;
       armed = false;
@@ -144,16 +159,23 @@ void updateDisplay() {
   lcd.setCursor(0, 0);
 
   if (currentMode == NORMAL) {
+
     if (alarmActive) {
       lcd.print("ALARM! GET UP! ");
       lcd.setCursor(0, 1);
       lcd.print("Press remote... ");
-    } else if (armed) {
-      unsigned long remaining = (alarmTargetMillis > millis()) ? (alarmTargetMillis - millis()) : 0;
+    } 
+
+    else if (armed) {
+      unsigned long remaining = (alarmTargetMillis > millis()) ? 
+                                (alarmTargetMillis - millis()) : 0;
+
       int remH = remaining / 3600000UL;
       int remM = (remaining % 3600000UL) / 60000UL;
       int remS = (remaining % 60000UL) / 1000UL;
+
       lcd.print("Ring in:        ");
+
       lcd.setCursor(0, 1);
       printTwoDigit(remH);
       lcd.print(":");
@@ -161,17 +183,23 @@ void updateDisplay() {
       lcd.print(":");
       printTwoDigit(remS);
       lcd.print("        ");
-    } else {
+    } 
+
+    else {
       lcd.print("Press SET to    ");
       lcd.setCursor(0, 1);
-      lcd.print("set (hold=1min) ");
+      lcd.print("set (hold=5sec) ");
     }
-  } else if (currentMode == SET_HOURS) {
+  } 
+
+  else if (currentMode == SET_HOURS) {
     lcd.print("Set Hours:      ");
     lcd.setCursor(0, 1);
     lcd.print(setHours);
     lcd.print(" hour(s)  NEXT>>");
-  } else if (currentMode == SET_MINUTES) {
+  } 
+
+  else if (currentMode == SET_MINUTES) {
     lcd.print("Set Minutes:    ");
     lcd.setCursor(0, 1);
     lcd.print(setMinutes);
